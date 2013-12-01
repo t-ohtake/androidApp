8d0e971f81a325f058902df668254fe40ad6c335
t-ohtake <t.ohtake@s2soft.co.jp>
2013-12-01 21:39:43 +0900

    ブラウザでの認証追加

--- src/com/example/androidapp/CallBackActivity.java
+++ src/com/example/androidapp/CallBackActivity.java
@@ -1,14 +1,13 @@
 package com.example.androidapp;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
-//コールバック専用クラス
-public class CallBackActivity extends Activity
-{
+public class CallBackActivity extends Activity {
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.callback);
 	}
